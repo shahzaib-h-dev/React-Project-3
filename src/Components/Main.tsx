cf1d3a01e11bb9f3069cb8865f527dd721e47b7a
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-08-23 14:32:44 +0500

    Map ingredients list

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,14 +1,33 @@
 export function Main(){
+    
+    const ingredients =["Chicken", "Oregano", "Tomatoes"]
+    const ingredientListItems = ingredients.map(ingredient =>(
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event : any){
+        event.preventDefault()
+        console.log("Form submitted!")
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient as string)
+        console.log(ingredients)
+
+    }
+
     return<>
    <main className="mainTag">
-    <form className="add-ingredient-form">
+    <form onSubmit={handleSubmit}  className="add-ingredient-form">
         <input 
+        name="ingredient"
          type="text"
          placeholder="e.g. oregano"
          aria-label="Add ingredient"/>
         <button> Add ingredient</button>
-
     </form>
+    <ul>
+       {ingredientListItems}
+    </ul>
    </main>
     
     </>
