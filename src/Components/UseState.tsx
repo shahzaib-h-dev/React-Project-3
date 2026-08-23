d7d37da1f2ee6925d547152903ee8030e6dc08f9
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-08-23 16:09:27 +0500

    Toggling state

--- src/Components/UseState.tsx
+++ src/Components/UseState.tsx
@@ -1,15 +1,20 @@
 import React from "react"
 
 export function UseState(){
+ 
+    const [isGoingOut ,setIsGoingOut] = React.useState(false)
 
-    const isGoingOut = true 
-    let answer = isGoingOut === true ? "Yes" : "No"
-
+    function changeMind(){
+   setIsGoingOut(prev => !prev)
+    }
+    
     return<>
     <main>
         <h1>Do i feel like going out tonight?</h1>
      
-        <button>{answer}</button>
+        <button onClick={changeMind}
+         aria-label={'Current answer is ${isGoingOut ? "Yes" : "No"}. Click to change it.'}
+        >{isGoingOut ? "Yes" : "No"}</button>
     </main>
 
     
