faac4fa1a9dd72337a3e87175c3a4864d0fcad2a
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-08-27 14:03:15 +0500

    Conditional rendering: ternary

--- src/Components/Jokes.tsx
+++ src/Components/Jokes.tsx
@@ -10,13 +10,11 @@ export function Jokes(props: any){
     }
 
     return(
-        <>
          <div>
             {props.setup && <h3>{props.setup}</h3> }
-            { isShown === true && <p>{props.punchline}</p>}
-            <button onClick={toggleShown}>Show punchline</button>
+            { isShown  ? <p>{props.punchline}</p> : null}
+            <button onClick={toggleShown}>{isShown ? "Hide" : "Show"} punchline</button>
+            <hr />
          </div>
-
-        </>
     )
 }
