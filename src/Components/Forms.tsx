7330974b69a91f7799fe82b23728975692f938c1
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-08-27 10:51:36 +0500

    Form submission

--- src/Components/Forms.tsx
+++ src/Components/Forms.tsx
@@ -1,25 +1,45 @@
-import React from "react"
+import React from "react";
 
-export function Forms(){
-    return(
-        <>
-         <section>
-            <h1>Signup Form</h1>
-           
-            <form> 
-                 <label htmlFor="email">Email: </label>
-                <input id="email" type="email" name="email" placeholder="shahzaib@gmail.com"/>
-                <br />
-                <br />
+export function Forms() {
 
-                <label htmlFor="">Password: </label>
-                <input id="password" type="password" name="password" />
-                <br />
-                <br />
-                <button>Submit</button>           
-           
+    function handleSubmit(event: any){
+        event.preventDefault()
+        const formEl = event.currentTarget
+        const formData = new FormData(formEl)
+        const email = formData.get("email")
+        console.log(email)
+        formEl.reset()
+
+    }
+
+    return (
+        <section className="signup-container">
+            <h1 className="signup-title">Signup form</h1>
+            
+            <form onSubmit={handleSubmit} className="signup-form" method="post"> 
+                <div className="input-group">
+                    <label htmlFor="email">Email:</label>
+                    <input 
+                        id="email" 
+                        type="email" 
+                        name="email" 
+                        placeholder="shahzaib@gmail.com" 
+                        className="signup-input"
+                    />
+                </div>
+
+                <div className="input-group">
+                    <label htmlFor="password">Password:</label>
+                    <input 
+                        id="password" 
+                        type="password" 
+                        name="password" 
+                        className="signup-input"
+                    />
+                </div>
+                
+                <button type="button" className="signup-button">Submit</button>           
             </form>
-         </section>
-        </>
-    )
+        </section>
+    );
 }
