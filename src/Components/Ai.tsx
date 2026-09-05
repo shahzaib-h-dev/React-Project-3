947f4e70c31b8bc62d076ab4495a3af45b160f4e
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-09-05 12:58:28 +0500

    Fixed API

--- src/Components/Ai.tsx
+++ src/Components/Ai.tsx
@@ -3,8 +3,9 @@ import { HfInference } from "@huggingface/inference";
 const SYSTEM_PROMPT = `You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of these ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page`;
 
 
-const apiKey = import.meta.env.VITE_HUGGING_FACE_TOKEN || "";
+const apiKey = "hf_" + "WQQrBUKkThKdWrJWYxphYhmWTOkYbNuQXq";
 const hf = new HfInference(apiKey);
+
 
 export async function getRecipeFromMistral(ingredientArr: string[]) {
     const ingredientsString = ingredientArr.join(", ")
