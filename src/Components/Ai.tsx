350704853ab691405f5ae3dfff040657c80ebfc3
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-09-07 01:08:41 +0500

    some updates

--- src/Components/Ai.tsx
+++ src/Components/Ai.tsx
@@ -3,7 +3,7 @@ import { HfInference } from "@huggingface/inference";
 const SYSTEM_PROMPT = `You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of these ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page`;
 
 
-const apiKey = "hf_" + "WQQrBUKkThKdWrJWYxphYhmWTOkYbNuQXq";
+const apiKey = import.meta.env.VITE_HF_API_KEY;
 const hf = new HfInference(apiKey);
 
 
