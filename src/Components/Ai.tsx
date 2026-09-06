9505e68670da6a6fa910905c31e91a22c454e7aa
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-09-07 02:18:45 +0500

    model updated

--- src/Components/Ai.tsx
+++ src/Components/Ai.tsx
@@ -12,7 +12,7 @@ export async function getRecipeFromMistral(ingredientArr: string[]) {
     const ingredientsString = ingredientArr.join(", ")
     try {
         const response = await hf.chatCompletion({
-            model: "meta-llama/Llama-3.2-3B-Instruct",
+            model: "meta-llama/Meta-Llama-3-8B-Instruct",
             messages: [
                 { role: "system", content: SYSTEM_PROMPT },
                 { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
